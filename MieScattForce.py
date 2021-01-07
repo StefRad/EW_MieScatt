57d726e05c7e8e1f39efa90886d622688183102a
DavideD96 <63403701+DavideD96@users.noreply.github.com>
2021-01-07 09:03:52 +0100

    Add files via upload

--- MieScattForce.py
+++ MieScattForce.py
@@ -29,7 +29,7 @@ class MieScattForce:
         
         #particle radius
         self.a = a;
-        self.b = a+1e-6;############################
+        self.b = a-0.999999;############################
         
         # distance from surface
         self.h = dist;
@@ -41,14 +41,15 @@ class MieScattForce:
         
         self.alpha = (n2*2*pi*a) / (lamb);
         self.bet   = (n2*2*pi*self.b) / (lamb);
-        self.beta  = ((n1*2*pi)/(lamb))*np.sqrt(sin(theta1)**2 - self.n21**2)
+        self.beta  = np.real(((n1*2*pi)/(lamb))*np.sqrt(sin(theta1)**2 - self.n21**2))
         self.gamma = (n1*2*pi/lamb)*sin(theta1);
         
         # transmission coefficient
         self.Tp = (2*self.n21*cos(theta1)) / (self.n21**2 * cos(theta1) + 1j*np.sqrt(sin(theta1)**2 - self.n21**2));
-        self.Ts = (2*cos(theta1)) / (cos(theta1) + 1j*np.sqrt(sin(theta1)**2 + self.n21**2));
+        self.Ts = (2*cos(theta1)) / (cos(theta1) + 1j*np.sqrt(sin(theta1)**2 - self.n21**2));
         
-        
+        print('b = ',self.b)
+        print('beta = ',self.beta)
         
     def alpha1(self,l,m):
         fatt1 = np.sqrt( ((2*l+1)*factorial(l-m)) / (4*pi*factorial(l+m)) )
@@ -58,16 +59,28 @@ class MieScattForce:
     
     def Q1(self,l,m):
         fatt = 2*pi*((-1)**(m-1))
-        if (l+m)%2 == 0:
-            def integrand(theta):
-                u = self.beta*self.b*sin(theta)
-                res = sin(theta)**2 * cos(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m-1),u)+Imz(abs(m+1),u))
-                return res
+        if (l+abs(m))%2 == 0:
+            if m > 0:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta)**2 * cos(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m-1),u)+Imz(abs(m+1),u))
+                    return res
+            else:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta)**2 * cos(self.gamma*self.b*cos(theta)) *(-1)**abs(m)* Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m-1),u)+Imz(abs(m+1),u))
+                    return res    
         else:
-            def integrand(theta):
-                u = self.beta*self.b*sin(theta)
-                res = sin(theta)**2 * (1j) * sin(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m-1),u)+Imz(abs(m+1),u))
-                return res
+            if m > 0:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta)**2 * (1j) * sin(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m-1),u)+Imz(abs(m+1),u))
+                    return res
+            else:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta)**2 * (1j) * sin(self.gamma*self.b*cos(theta)) *(-1)**abs(m)* Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m-1),u)+Imz(abs(m+1),u))
+                    return res
             
         def real_part(theta):
             return np.real(integrand(theta))
@@ -81,16 +94,28 @@ class MieScattForce:
     
     def Q2(self,l,m):
         fatt = 4*pi*((-1)**(m))
-        if (l+m)%2 == 0:
-            def integrand(theta):
-                u = self.beta*self.b*sin(theta)
-                res = sin(theta) * cos(theta) * (1j) * sin(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
-                return res
+        if (l+abs(m))%2 == 0:
+            if m > 0:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta) * cos(theta) * (1j) * sin(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
+                    return res
+            else:    
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta) * cos(theta) * (1j) * sin(self.gamma*self.b*cos(theta)) * (-1)**abs(m)*Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
+                    return res
         else:
-            def integrand(theta):
-                u = self.beta*self.b*sin(theta)
-                res = sin(theta) * cos(theta) * cos(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
-                return res
+            if m > 0:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta) * cos(theta) * cos(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
+                    return res
+            else:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta) * cos(theta) * cos(self.gamma*self.b*cos(theta)) * (-1)**abs(m)*Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
+                    return res
         def real_part(theta):
             return np.real(integrand(theta))
         def imag_part(theta):
@@ -104,16 +129,27 @@ class MieScattForce:
     def Q3(self,l,m):
         fatt = 4*pi*1j*((-1)**(m))*(m / (self.beta*self.b)) ######################
         if (l+m)%2 == 0:
-            def integrand(theta):
-                u = self.beta*self.b*sin(theta)
-                res = sin(theta) * cos(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
-                return res
+            if m > 0:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta) * cos(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
+                    return res
+            else:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta) * cos(self.gamma*self.b*cos(theta)) * (-1)**abs(m)* Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
+                    return res
         else:
-            def integrand(theta):
-                u = self.beta*self.b*sin(theta)
-                res = sin(theta) * (1j) * sin(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
-                return res
-        
+            if m > 0:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta) * (1j) * sin(self.gamma*self.b*cos(theta)) * Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
+                    return res
+            else:
+                def integrand(theta):
+                    u = self.beta*self.b*sin(theta)
+                    res = sin(theta) * (1j) * sin(self.gamma*self.b*cos(theta)) * (-1)**abs(m)* Legendre(m,l,cos(theta))[0][m,l]*(Imz(abs(m),u))
+                    return res
         def real_part(theta):
             return np.real(integrand(theta))
         def imag_part(theta):
@@ -140,6 +176,8 @@ class MieScattForce:
             k = i+1
             for j in range(2*k+1):
                 m = j-k;
+                print(m)
+                print((sin(self.theta1)*self.Q1(k,m))-((1j)*np.sqrt(sin(self.theta1)**2 - self.n21**2)*self.Q2(k,m)))
                 Ap1.append((self.alpha1(k,m)/self.n21) * self.Tp * exp(-self.beta*self.h) * ((sin(self.theta1)*self.Q1(k,m))-((1j)*np.sqrt(sin(self.theta1)**2 - self.n21**2)*self.Q2(k,m))))
                 Bp1.append(self.n2*self.alpha1(k,m) * self.Tp * exp(-self.beta*self.h) * self.Q3(k,m))
                 As1.append((self.Ts / (self.n2*self.Tp)) * self.n2*self.alpha1(k,m) * self.Tp * exp(-self.beta*self.h) * self.Q3(k,m))
@@ -156,6 +194,8 @@ def Imz(m,z):
     arg = 1j*z
     res = ((1j)**(-m)) * jv(m,arg)
     # res = iv(m,z)
-    print(res)
+    #print(res)
     return res
 
+def integr(theta):
+    return np.exp(1.4*np.cos(theta))*np.cos(2*theta)
