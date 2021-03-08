eb0a2aee2c71f702750c868c20045ef703aea792
StefRad <63404251+StefRad@users.noreply.github.com>
2021-03-08 17:10:48 +0100

    Update integration_time.py

--- integration_time.py
+++ integration_time.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from math import sin, cos,pi, atan2
 import lock_in_amplifier as lock
 
-amp = 2e-6;
+amp = 2e-7;
 
 phi = 1e-6;
 
@@ -21,7 +21,7 @@ f = 1e-5;
 
 lun = 10;
 
-npts = np.linspace(100,100000,lun)
+npts = np.linspace(100000,100000000,lun)
 
 tempo_campionamento = 1e-6;
 
@@ -37,17 +37,17 @@ phi_ = [];
 for i in range(lun):
     amp_i = [];
     phi_i = [];
-    for j in range(10):
-        l = lock.lock_in_amplifier(amp**2, amp, phi, int(npts[i]), tempo_campionamento, f);
+    for j in range(30):
+        l = lock.lock_in_amplifier(amp**2, 3e-2*amp, phi, int(npts[i]), tempo_campionamento, f);
         l.mixing();
         amp_j,phi_j = l.filtering(0, 0);
         amp_i.append(amp_j);
         phi_i.append(phi_j);
-    amp_.append(np.std(amp_i));
-    phi_.append(np.std(phi_i));
+    amp_.append(100*np.std(amp_i)/np.mean(amp_i));
+    phi_.append(100*np.std(phi_i)/np.mean(phi_i));
     print(i)
 
 plt.figure();
 
-plt.semilogy(t,amp_);
+plt.semilogx(t,amp_);
     
